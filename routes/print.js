2832337e5f0b2011b7fe673dda983ac1488ebc74
dalborgo <marco@dalborgo.it>
2017-09-13 19:14:00 +0200

    asten due

--- routes/print.js
+++ routes/print.js
@@ -1,27 +1,42 @@
+
 var config = require("../config/config.json");
-var escpos = require('escpos');
-var device  = new escpos.Network(config.printer.ip);
-var printer = new escpos.Printer(device);
+var escpos = require('escpos')
 var appRouter = function (app) {
     app.get("/api/print", function (req, res, next) {
+        ;
+        var device  = new escpos.Network(config.printer.ip);
+        var printer = new escpos.Printer(device);
         if (!req.user) {
-            res.render("secure", {expressFlash: req.flash('Nessun utente collegato!')});
-        } else {
-            device.open(function(){
+            return res.render("secure", {expressFlash: req.flash('Nessun utente collegato!')});
+        }
+        var obj ={
+            data : new Date(),
+            az: 'Asten',
+            user: req.user.username,
+            prod: 'Miscellaneus',
+            quant: '1.000',
+            costo: '18.00 ¯',
+            subtotale: '18.00 ¯',
+            totla: '18.00 ¯',
+            cash: '20.00 ¯',
+            change: '2.00 ¯'
+        };
+        device.open(function(){
                 printer
-                    .encode('850')
+                    .encode('857')
                     .font('a')
                     .align('ct')
-                    .style('bu')
                     .size(1, 1)
-                    .text(req.user.username).println()
-                    .text(req.user.createdON).feed(3)
-                printer.control("LF")
-                    .cut()
-                    .close();
+                    .text(obj.data).println()
+                    .align('lt')
+                    .text(obj.az).text(obj.user).println()
+                    .text(obj.prod+'\t\t'+obj.quant+'      '+obj.costo).println()
+                    .text('Subtotal\t\t\t'+obj.subtotale).size(1, 2).text('Total:\t\t\t'+obj.totla).println().size(1,1)
+                    .text('Cash (EUR)\t\t\t'+obj.cash).println()
+                    .text('Change:\t\t\t'+obj.change).close()
             });
             res.json(req.user)
-        }
+
     });
 };
 module.exports = appRouter;
